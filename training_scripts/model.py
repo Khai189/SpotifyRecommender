0f5f91884556ecdf62ace60d99707be2ab1e9307
Khai189 <khmr2024@mymail.pomona.edu>
2025-12-03 14:10:11 -0800

    updating project

--- training_scripts/model.py
+++ training_scripts/model.py
@@ -3,10 +3,26 @@ from torch import nn
 import torchaudio
 from torch.utils.data import Dataset, DataLoader
 from torchaudio import datasets, transforms
-device = "cuda" if torch.cuda.is_available() else "cpu"
+if torch.cuda.is_available():
+    device = "cuda"
+    print("Cuda device is available")
+elif not torch.backends.mps.is_available():
+    device= "cpu"
+    if not torch.backends.mps.is_built():
+        print("MPS not available because the current PyTorch install was not "
+              "built with MPS enabled.")
+    else:
+        print("MPS not available because the current MacOS version is not 12.3+ "
+              "and/or you do not have an MPS-enabled device on this machine.")
+
+else:
+    device = "mps"
+    print("Mac device available")
 
 class SongAnalyzer(nn.Module):
     def __init__(self,
-                 ):
+                 input_size: int,
+                 hidden_units: int,
+                 output_units: int):
         super().__init__()
 
