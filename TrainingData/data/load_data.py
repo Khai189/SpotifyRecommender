70dca6130f96931a8f9f95e2c80cbda6414b6e6c
Khai189 <khmr2024@mymail.pomona.edu>
2025-12-03 14:09:09 -0800

    updating project

--- TrainingData/data/load_data.py
+++ TrainingData/data/load_data.py
@@ -1,6 +1,15 @@
 import os
 import torchaudio
 import pandas as pd
+from sqlalchemy import create_engine
+from sqlalchemy import text
+import seaborn as sns
+import seaborn.objects as so
+import matplotlib.pyplot as plt
+import psycopg2
 files = [file for file in os.listdir("mp3_files")]
-data = pd.DataFrame(files)
-print(data)
+df = pd.read_csv('dataset.txt', sep=',', header=0)
+print(df.columns)
+
+engine = create_engine('postgresql://husamkm:#Kha704293@localhost/spotify', echo=False)
+df.to_csv('dataset.csv', sep=',',index=False, encoding='utf-8')
--- TrainingData/data/scraper.py
+++ TrainingData/data/scraper.py
@@ -1,24 +1,10 @@
-from concurrent.futures import ThreadPoolExecutor, as_completed
-import spotify_scraper
-import json
-from pathlib import Path
-from spotify_scraper import SpotifyClient
-from spotify_scraper.extractors import (
-    TrackExtractor,
-    AlbumExtractor,
-    ArtistExtractor,
-    PlaylistExtractor
-)
-from spotify_scraper.parsers import json_parser
-# Set up our client
-client = SpotifyClient()
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import os
 
-# Run a test case for a track before we start generating our data
-track_url = "https://open.spotify.com/track/61mWefnWQOLf90gepjOCb3"
-track = client.get_track_info(track_url)
-preview_path = client.download_preview_mp3(track_url,
-                                           path=Path("mp3_files"))
-artist = track.get('artists', [])[0].get("name")
-with open(f"mp3_files/{track["name"]}_{artist}.json", "w") as f:
-    f.write(json.dumps(track))
-# json_parser.extract_track_data()
+from scraper_auth import sp
+results = sp.current_user_saved_tracks()
+for idx, item in enumerate(results['items']):
+    track = item['track']
+    print(idx, track['artists'][0]['name'], " – ", track['name'])
+
